b7b99e8701cd82448a89a3e16e5d0ea178ea774b
hongmoSung <tesung30@naver.com>
2022-05-28 21:54:07 +0900

    initial commit

---
+++ src/main/java/toy/batch/ToyBatchApplication.java
@@ -0,0 +1,13 @@
+package toy.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/batch/ToyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
